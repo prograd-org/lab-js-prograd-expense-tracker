5ac0e1156daa1a867450a4ee35db8df92b7d6b7e
Gurshaan7869 <gurshaan7869@gmail.com>
2022-02-24 15:31:07 +0530

    19BCE10041

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-
+function addTransaction(){
+    var myvar = {
+        id: localStorage.length,
+        name: document.getElementById("text").value,
+        amount: document.getElementById("amount").value
+    };
+    console.log(myvar.id)
+    if(myvar.name && myvar.amount)
+        localStorage.setItem(myvar.id, JSON.stringify(myvar))
+}
+var totpos = 0.00, totneg = 0.00, balance = 0.00
+len = localStorage.length 
+for (let i = len; i>0; i--) {
+    var item = JSON.parse(localStorage.getItem(i-1))
+    var li = document.createElement("li")
+    if(item.amount<0){
+        li.classList.add("minus")
+        totneg = totneg + parseFloat(item.amount)
+        totneg.toFixed(2)
+    }
+    else {
+        li.classList.add("plus")
+        totpos = totpos + parseFloat(item.amount)
+    }
+    var ul = document.getElementById("list")
+    ul.appendChild(li)
+    li.innerHTML = "<div>"+item.name+"</div> <div>"+item.amount+"</div>" 
+}
+balance = totneg + totpos
+totneg = totneg.toFixed(2)
+totpos = totpos.toFixed(2)
+balance = balance.toFixed(2)
+document.getElementById("balance").innerHTML = "$"+balance
+document.getElementById("money-plus").innerHTML = "+$"+totpos
+document.getElementById("money-minus").innerHTML = "-$"+totneg
